f683c7043c93d4b74cd44629c8db35e523d1cf13
Abdur Rahman Bin Sharif <asharif1@usf.edu>
2023-09-23 01:57:38 -0400

    automated gcp upload

--- server/downloader.py
+++ server/downloader.py
@@ -1,28 +1,8 @@
-import instaloader
+import subprocess
 
-def download_first_post(target_username):
-    print("Initializing Instaloader instance.")
-    
-    # Get instance
-    L = instaloader.Instaloader()
+def run_instaloader():
+    cmd = ["python", "-m", "instaloader", "--login=lwas_2023", ":saved"]
+    subprocess.run(cmd)
 
-    print(f"Attempting to retrieve posts for {target_username}.")
-    
-    # Initialize the Profile from a username
-    profile = instaloader.Profile.from_username(L.context, target_username)
-    
-    # Iterate over posts and download only the first post
-    for index, post in enumerate(profile.get_posts()):
-        if index == 0:  # First post
-            print(f"Found the first post with the shortcode: {post.shortcode}. Downloading...")
-            L.download_post(post, target=target_username)
-            print(f"Downloaded the first post from {target_username}.")
-            break  # Exit the loop as we've downloaded the first post
-        else:
-            print("No posts found!")
-            
-if __name__ == '__main__':
-    target_username = 'some_public_profile_username'  # Replace with the target username
-    print(f"Starting the download process for {target_username}.")
-    download_first_post(target_username)
-    print("Download complete.")
+if __name__ == "__main__":
+    run_instaloader()
---
+++ server/uploader.py
@@ -0,0 +1,60 @@
+import os
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from google.cloud import storage  # make sure to install google-cloud-storage package
+
+class Handler(FileSystemEventHandler):
+    def process(self, event):
+        # Extract file extension
+        filename = os.path.basename(event.src_path)
+
+        if event.is_directory:
+            print(f"Received directory event - {event.src_path}.")
+            return
+        elif event.event_type == 'created' and 'mp4' in filename:
+            print(f"Received created event for .mp4 file - {event.src_path}.")
+            # Wait for 2 seconds before starting the upload
+            time.sleep(10)
+            blob_name = event.src_path.replace('.temp', '')
+            bucket_name = "lightworkarabicseason"  # Replace with your bucket name
+            storage_client = storage.Client.from_service_account_json('cred/creds.json')  # Replace with your credentials path
+            bucket = storage_client.get_bucket(bucket_name)
+            blob = bucket.blob(os.path.basename(blob_name))
+
+            blob.upload_from_filename(blob_name)
+
+            print(f"File {blob_name} uploaded to {bucket_name}.")
+        else:
+            print(f"Ignored non-.mov event - {event.src_path}.")
+
+    def on_created(self, event):
+        self.process(event)
+
+import threading
+
+def print_files_in_directory(directory_path):
+    while True:
+        time.sleep(10)
+        with os.scandir(directory_path) as dir_entries:
+            files = [entry.name for entry in dir_entries if entry.is_file()]
+            print(f"Current list of .mp4 files in {directory_path}: {files}")
+
+if __name__ == "__main__":
+    observer = Observer()
+    event_handler = Handler()
+    directory_path = '：saved'  # Replace with the path of your directory
+    observer.schedule(event_handler, path=directory_path, recursive=False)
+    observer.start()
+    
+    # # Start a new thread that prints the list of files every 10 seconds
+    # list_files_thread = threading.Thread(target=print_files_in_directory, args=(directory_path,))
+    # list_files_thread.start()
+
+    try:
+        while True:
+            time.sleep(2)
+    except KeyboardInterrupt:
+        observer.stop()
+    
+    observer.join()
